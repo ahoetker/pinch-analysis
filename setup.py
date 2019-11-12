c042b9478f8d60ed45bcd7e68275ff97fe756543
Andrew Hoetker <ahoetker@me.com>
2019-11-11 17:50:31 -0700

    add tests and packaging
    Signed-off-by: ahoetker <ahoetker@me.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pinch-analysis",
+    version="0.1.0",
+    description="Pinch-point technique for heat integration analysis in chemical plants.",
+    url="https://github.com/ahoetker/pinch-analysis",
+    author="Andrew Hoetker, Emma Holle, James Taylor, Steve Wilson",
+    author_email="ahoetker@me.com",
+    packages=find_packages(),
+    zip_safe=False,
+)
--- tests/test_plots.py
+++ tests/test_plots.py
@@ -12,7 +12,7 @@ from pinch.plots import (
 def test_cold_composite():
     cold_temp = Q_(np.array([30.30, 106.70, 240.00]), "celsius")
     enth = Q_(np.array([187479.5040, 246613.1040, 453441.3840]), "MJ")
-    cold_composite(enth, cold_temp, show=True)
+    cold_composite(enth, cold_temp, show=False)
 
 
 def test_grand_composite():
@@ -35,7 +35,7 @@ def test_grand_composite():
         ),
         "MJ",
     )
-    grand_composite(enth, temp, show=True)
+    grand_composite(enth, temp, show=False)
 
 
 def test_combined_composite():
@@ -44,10 +44,10 @@ def test_combined_composite():
     hot_enth = Q_(np.array([0, 3.259609e4, 294112.728, 384813.576, 437925.384]), "MJ")
     hot_temp = Q_(np.array([45.00, 52.10, 159.20, 206.10, 240.00]), "celsius")
     enth = Q_(np.array([0, 3.259609e4, 294112.728, 384813.576, 437925.384]), "MJ")
-    combined_composite(cold_enth, hot_enth, cold_temp, hot_temp, show=True)
+    combined_composite(cold_enth, hot_enth, cold_temp, hot_temp, show=False)
 
 
 def test_hot_composite():
     hot_temp = Q_(np.array([45.00, 52.10, 159.20, 206.10, 240.00]), "celsius")
     enth = Q_(np.array([0, 3.259609e4, 294112.728, 384813.576, 437925.384]), "MJ")
-    hot_composite(enth, hot_temp, show=True)
+    hot_composite(enth, hot_temp, show=False)
